bfce0ec40035fa321f35efa0e774a566e21c0f99
kingcavespider1 <40778428+kingcavespider1@users.noreply.github.com>
2025-06-04 13:07:01 -0400

    Alpaquita linux test builds (#3)
    * alpaquita support for testing
    
    * Update build-multiarch.yml

---
+++ build/alpaquita/install-packages.sh
@@ -0,0 +1,53 @@
+#!/bin/sh
+
+set -e
+set -o pipefail
+
+apk add alpine-keys --repository=http://dl-cdn.alpinelinux.org/alpine/edge/main/ --no-cache -U --allow-untrusted
+echo "@alpine_main https://dl-cdn.alpinelinux.org/alpine/edge/main" >> /etc/apk/repositories
+echo "@alpine_community https://dl-cdn.alpinelinux.org/alpine/edge/community" >> /etc/apk/repositories
+apk update
+
+# Install necessary packages
+# shellcheck disable=SC2086
+apk add --no-cache -U \
+    openssl \
+    imagemagick@alpine_community \
+    file \
+    lsof \
+    su-exec@alpine_main \
+    coreutils \
+    findutils \
+    procps \
+    shadow \
+    bash \
+    curl \
+    iputils \
+    git \
+    jq \
+    mysql-client \
+    tzdata \
+    rsync \
+    nano \
+    ncurses \
+    sudo \
+    tar \
+    zstd \
+    nfs-utils \
+    libpcap \
+    libwebp \
+    libcap \
+    ${EXTRA_ALPINE_PACKAGES}
+
+# Download and install patched knockd
+curl -fsSL -o /tmp/knock.tar.gz https://github.com/Metalcape/knock/releases/download/0.8.1/knock-0.8.1-alpine-amd64.tar.gz
+tar -xf /tmp/knock.tar.gz -C /usr/local/ && rm /tmp/knock.tar.gz
+ln -s /usr/local/sbin/knockd /usr/sbin/knockd
+setcap cap_net_raw=ep /usr/local/sbin/knockd
+
+# Set Git credentials globally
+cat <<EOF >> /etc/gitconfig
+[user]
+	name = Minecraft Server on Docker
+	email = server@example.com
+EOF
